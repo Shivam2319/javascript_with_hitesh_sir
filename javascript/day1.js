513d08ecb33800680b0981216d7e995fe5215ebf
shivam kumar <117751090+Shivam2319@users.noreply.github.com>
2023-08-09 04:42:31 +0000

    study let var and const

--- javascript/day1.js
+++ javascript/day1.js
@@ -1,1 +1,18 @@
-console.log("hello i am shivam");
+const accountId=25252552;
+let accountEmail = "skshiv197@gmail.com";
+var accountPassword ="65896";
+accountCity= "Darbhanga";
+// const accountId=52552;
+accountEmail = "skshiv197@hotmail.com";
+accountPassword ="696";
+accountCity= "Dhanbad";
+console.table([accountId,accountEmail,accountPassword, accountCity])
+
+/* notes**********notes
+    when we declare variable we use   Let and Const
+    let:- let is use for declare variable that will be updated in futere that means it is muted in nature
+    const:- const is use for declare variable that never be updated in future
+    
+    plz dont use var because of issues in block scope and functional scope
+    */
+   
