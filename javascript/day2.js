aa26ac166fcd9dd42ec1bb108fb4cce24caeb65e
shivam kumar <117751090+Shivam2319@users.noreply.github.com>
2023-08-19 20:40:18 +0000

    datatypes

---
+++ javascript/day2.js
@@ -0,0 +1,22 @@
+// Datatypes and Ecma Standards
+"use strict"; //this keyword is define for treat all js code as newer version
+
+// alert("hello"); //alert function is used for show a popup with your msg write under the "" and this will only show o browser not in CLI or node
+
+
+/* DataTypes:-
+    number=> range (2 to the power 53)-1
+    bigint
+    string=> " write under double or single quotes"
+    boolean => true/false
+    null=> standalone value
+    undefined => when only variableis defined and the value of variable is not defined eg. let population;
+    symbol=>*/
+    let day = "sunday";
+    let date=20;
+    let month= "august";
+    let year = 2023;
+    console.log(typeof (date)); //typeof is used for knowing variable datatypes
+    console.log(typeof ("hitesh")); //we also write typeof like this 
+    console.log(typeof undefined); //output:- undefined
+    console.log(typeof null); //output:- object
