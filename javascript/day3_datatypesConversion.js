21bbe35918595efb921c0b1967bb44058d8d66e3
shivam kumar <117751090+Shivam2319@users.noreply.github.com>
2023-08-19 20:56:39 +0000

    datatype conversion

---
+++ javascript/day3_datatypesConversion.js
@@ -0,0 +1,9 @@
+let score=33;
+console.log(typeof score); //output:- number
+let section = "9A"
+console.log(typeof(section)); //output:- string
+
+let valueInNumber = Number(section);
+console.log(typeof valueInNumber);//output:- number
+
+console.log(valueInNumber); //output:- NaN
