b37aca5c690973c5d129925e6a9c2936f2483176
shivam kumar <117751090+Shivam2319@users.noreply.github.com>
2023-08-26 19:22:21 +0000

    conversion ofdatatypes

--- javascript/day3_datatypesConversion.js
+++ javascript/day3_datatypesConversion.js
@@ -7,3 +7,17 @@ let valueInNumber = Number(section);
 console.log(typeof valueInNumber);//output:- number
 
 console.log(valueInNumber); //output:- NaN
+
+let myName = "shivam";
+console.log(myName);
+ let valueInString = Number(myName);
+ console.log(valueInString);
+ console.log(typeof(valueInString));
+
+ let isloggedin = 1;
+ let booleanisloggedin = Boolean(isloggedin);
+ console.log(booleanisloggedin); 
+
+//  1=> true;  0=> false
+// "" => false
+// "shivam" => true
