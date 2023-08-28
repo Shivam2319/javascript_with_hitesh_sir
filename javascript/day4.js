956fa2af68e5083f1a64697995d997eda534f175
shivam kumar <117751090+Shivam2319@users.noreply.github.com>
2023-08-28 15:18:41 +0000

    datatypes and its types

---
+++ javascript/day4.js
@@ -0,0 +1,44 @@
+// ****************operations****************
+let value = 3;
+let negValue = -value ; //this will create negative of value  
+console.log(negValue); // output is -3
+
+/*  Types of javascript operator:-
+(1)Arithmetic Operators
+(2)Comparison (Relational) Operators
+(3)Bitwise Operators
+(4)Logical Operators
+(5)Assignment Operators
+(6)Special Operators
+*/
+
+// (1) Arithmetic operator:- Arithmatic operator are used to perform arthematic operation. there are many types of arithmatic operators they are:-
+// (a) addition :- This operator is used to sum two or more values. It is denoted by +
+// example:-
+                    /*let a=20;
+                    let b=40;
+                    let sum = a+b;
+                    console.log(sum);*/
+// (b)subtraction :- This operator is used to subtract two or more values. It is denoted by -
+// (c) Multiplication :- This operator is used to multiply two or more values. It is denoted by *
+// (d) power :- This operator is used to find power of any values. It is denoted by **
+// example
+                /*let power=5**2
+                console.log(power);*/
+// (e) division  :- This operator is used to devide numbers. It is denoted by /
+// (f) modulus(reminder)  :- This operator is used to find remainder. It is denoted by %
+// (g) incerement :- this is used to iterate the value by 1. it is denoted by ++
+// (g) decrement:- this is used to iterate the value by -1. it is denoted by --
+
+
+// **********(2) Comparison (relational) operator
+// The JavaScript comparison operator compares the two operands. The comparison operators are as follows:
+/* Operator     	    Description	                                 Example
+    ==	                Is equal to	                                10==20 = false
+    ===	            Identical (equal and of same type)	            10==20 = false
+    !=	                Not equal to	                            10!=20 = true
+    !==	                Not Identical	                            20!==20 = false
+    >	                Greater than	                            20>10 = true
+    >=	                Greater than or equal to	                20>=10 = true
+    <	                Less than	                                20<10 = false
+    <=	                Less than or equal to	                    20<=10 = false */
---
+++ javascript/day5.js
@@ -0,0 +1,12 @@
+/*on the basisof accessing and locating there are two types of datatypes:-
+(1). primitive datatypes
+(2). non primitive datatypes
+(1). primitive datatypes(call by value):- There are seven types of primitive data types in JavaScript. They are as follows:
+(a). String (b). Number (c). Boolean  (d).Null (e). Undefined (f). Symbol (g). BigInt
+
+(2). Non-primitive datatypes(refrence):- 
+
+(a)Object (b) Array (c) Functions
+time 1ghanta 46 min
+
+*/
