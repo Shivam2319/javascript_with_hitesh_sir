918c1f05eddccfe3ef0aed21d84124f90b3b7caa
shivam kumar <117751090+Shivam2319@users.noreply.github.com>
2023-09-29 15:38:14 +0000

    object complete

--- javascript/03objects.js
+++ javascript/03objects.js
@@ -36,3 +36,63 @@ jsUser.greeting = function(){
 jsUser.greetingTwo = function(){
     console.log(`hello js user, ${this.name}` );
 }
+
+
+
+const tinderUser = {} // both are same but the major difference between this is Non singleton object
+
+// const tinderUser1 = new Object() //this is singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "shivam"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "skshiv@google.com",
+        fullName: {
+            userFullName :{
+                firstName: "Shivam",
+                lastName : "tiwari"
+            }
+        }
+}
+// console.log(regularUser.fullName.userFullName.firstName);
+
+let obj1 = {name:"shyam" ,age:28}
+let obj2 = {fathername:"shyam_lal" ,age:48}
+
+// to combine obj1 and obj2
+// let obj3 = Object.assign({}, obj1, obj2);
+// let obj4 = Object.assign( obj1, obj2);
+// console.log(obj4);
+// console.log(obj3);
+
+// to combine two or more object using spread method
+let obj5 = {...obj1, ...obj2}
+
+// console.log(obj5);
+
+// accesing object by keys , value and enteries
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// when we access object by keys ,it converts the objects into arrays
+
+
+// .hasOwnProperty("question to be asked") is used to check the given property is available or not in the given object
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+                    // object de structured
+
+    const course = {
+        courseName : "sigma 1.0 full stack web devlopment batch",
+        price: "999",
+        courseTeacher: "hitesh Chaudhary"
+    }
+    // console.log(course.courseTeacher);
+
+    const {courseTeacher : teacher} = course
+    // console.log(courseTeacher); //when we destructure any object this coomand doesnt work
+    console.log(teacher);
